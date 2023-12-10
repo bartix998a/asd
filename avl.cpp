84a22e0babbdf20ace8b4561b605c27e6401cd86
bartek <bartek998a@gmail.com>
2023-12-10 16:26:34 +0100

    Insert.

--- avl.cpp
+++ avl.cpp
@@ -1,3 +1,124 @@
 //
 // Created by Bartek on 10.12.2023.
 //
+#include <stdexcept>
+
+class Node{
+public:
+    /*
+     * leftSon->val < val < righjtSon-val
+     */
+    Node* leftSon;
+    Node* rightSon;
+    size_t val;
+    int height;
+    Node(size_t minIndex, unsigned int x, unsigned int count) : leftSon(nullptr), rightSon(nullptr), height(0){};
+
+    void calculateHeight(){
+        height = 1 + std::max(rightSon->height, leftSon->height);
+    }
+
+    int bFactor(){
+        return leftSon->height - rightSon->height;
+    }
+
+    Node* leftRotation(){
+        Node* temp1 = rightSon->leftSon;
+        Node* newParent = rightSon;
+        rightSon->leftSon = this;
+        rightSon = temp1;
+        this->calculateHeight();
+        newParent->calculateHeight();
+        return newParent;
+    }
+
+    Node* rightRotation(){
+        Node* temp1 = leftSon->rightSon;
+        Node* newParent = leftSon;
+        leftSon->rightSon = this;
+        leftSon = temp1;
+        this->calculateHeight();
+        newParent->calculateHeight();
+        return newParent;
+    }
+
+    Node* doubleRightRotation(){
+        leftSon = leftSon->leftRotation();
+        return this->rightRotation();
+    }
+
+    Node* doubleLeftRotation(){ //might be a problem with nulls
+        rightSon = rightSon->rightRotation();
+        return this->leftRotation();
+    }
+
+    size_t max(){
+        if (this->rightSon != nullptr){
+            return rightSon->max();
+        }
+        return val;
+    }
+
+    size_t min(){
+        if (this->leftSon != nullptr){
+            return leftSon->min();
+        }
+        return val;
+    }
+
+    Node* insert(Node* node){
+        if (node->val == this->val) {
+            throw std::invalid_argument("Wartosc istnieje");
+        }
+        if (node->val < this->val){
+            if (leftSon == nullptr){
+                leftSon = node;
+            } else {
+                leftSon = leftSon->insert(node);
+            }
+            if (bFactor() == 2 && leftSon->bFactor() > 0){
+                return leftRotation();
+            } else if (bFactor() == 2 && leftSon->bFactor() < 0){
+                return doubleRightRotation();
+            }
+        } else {
+            if(rightSon == nullptr){
+                rightSon = node;
+            } else {
+                rightSon = rightSon->insert(node);
+            }
+            if (bFactor() == -2 && rightSon->bFactor() > 0){
+                return rightRotation();
+            } else if (bFactor() == -2 && rightSon->bFactor() < 0){
+                return  doubleLeftRotation();
+            }
+        }
+
+    };
+
+    Node* remove(size_t pos){
+        /* algorithm for removing*/
+    }
+
+    unsigned int get(size_t pos){
+        // pos should always be in the tree
+        if (pos < val){
+            if (leftSon != nullptr){
+                return leftSon->get(pos);
+            }
+            throw std::invalid_argument("Nie ma w drzewie");
+        } else if (pos > val) {
+            if (rightSon != nullptr){
+                return rightSon->get(pos);
+            }
+            throw std::invalid_argument("Nie ma w drzewie");
+        } else {
+            return val;
+        }
+    }
+};
+
+
+int main(void){
+    return 0;
+}
