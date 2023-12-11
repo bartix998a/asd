c3333fba1b3e5779e2eb83d9af4f5310127fcc5e
bartek <bartek998a@gmail.com>
2023-12-11 20:11:37 +0100

    Avl working hopefully.

--- avl.cpp
+++ avl.cpp
@@ -2,6 +2,14 @@
 // Created by Bartek on 10.12.2023.
 //
 #include <stdexcept>
+#include <queue>
+#include <iostream>
+#include <ranges>
+#include <list>
+#include <algorithm>
+#include <random>
+#include <stack>
+#include <cassert>
 
 class Node{
 public:
@@ -12,14 +20,14 @@ public:
     Node* rightSon;
     size_t val;
     int height;
-    Node(size_t minIndex, unsigned int x, unsigned int count) : leftSon(nullptr), rightSon(nullptr), height(0){};
+    Node(size_t val) : leftSon(nullptr), rightSon(nullptr), val(val), height(1){};
 
     void calculateHeight(){
         height = 1 + std::max(rightSon == nullptr ? 0 : rightSon->height, leftSon == nullptr ? 0 : leftSon->height);
     }
 
     int bFactor(){
-        return leftSon->height - rightSon->height;
+        return (leftSon == nullptr ? 0 : leftSon->height) - (rightSon == nullptr ? 0 : rightSon->height);
     }
 
     Node* leftRotation(){
@@ -52,6 +60,18 @@ public:
         return this->leftRotation();
     }
 
+    Node* balanceTree(){
+        if (bFactor() == -2 && rightSon->bFactor() <= 0){
+            return leftRotation();
+        } else if (bFactor() == -2 && rightSon->bFactor() > 0){
+            return  doubleLeftRotation();
+        } else if (bFactor() == 2 && leftSon->bFactor() >= 0){
+            return rightRotation();
+        } else if (bFactor() == 2 && leftSon->bFactor() < 0){
+            return doubleRightRotation();
+        } else return this;
+    }
+
     size_t max(){
         if (this->rightSon != nullptr){
             return rightSon->max();
@@ -66,6 +86,10 @@ public:
         return val;
     }
 
+    Node* insert(size_t pos){
+        return insert(new Node(pos));
+    }
+
     Node* insert(Node* node){
         if (node->val == this->val) {
             throw std::invalid_argument("Wartosc istnieje");
@@ -73,85 +97,169 @@ public:
         if (node->val < this->val){
             if (leftSon == nullptr){
                 leftSon = node;
+                calculateHeight();
+                return this;
             } else {
                 leftSon = leftSon->insert(node);
             }
-            if (bFactor() == 2 && leftSon->bFactor() > 0){
-                return leftRotation();
-            } else if (bFactor() == 2 && leftSon->bFactor() < 0){
-                return doubleRightRotation();
-            }
+            calculateHeight();
+            return balanceTree();
         } else {
             if(rightSon == nullptr){
                 rightSon = node;
+                calculateHeight();
+                return this;
             } else {
                 rightSon = rightSon->insert(node);
             }
-            if (bFactor() == -2 && rightSon->bFactor() > 0){
-                return rightRotation();
-            } else if (bFactor() == -2 && rightSon->bFactor() < 0){
-                return  doubleLeftRotation();
-            }
+            calculateHeight();
+            return balanceTree();
         }
+        calculateHeight();
         return this;
     };
 
-    // Assuming the tree contains pos
-    Node* remove(size_t pos){
-        if (pos < val){
-            leftSon = leftSon->remove(pos);
-            if (bFactor() == -2 && rightSon->bFactor() <= 0){
-                return leftRotation();
-            } else if (bFactor() == -2 && rightSon->bFactor() == 1){
-                return doubleLeftRotation();
-            }
-        } if (pos > val) {
-            rightSon = rightSon->remove(pos);
-            if (bFactor() == 2 && leftSon->bFactor() >= 0){
-                return rightRotation();
-            } else if (bFactor() == 2 && rightSon->bFactor() == -1){
-                return doubleRightRotation();
-            }
-        } else {
-            if (rightSon == nullptr) {
-                return leftSon;
-            } else if (leftSon == nullptr) {
-                return rightSon;
-            }
-            Node* temp;
-            if (bFactor() == 1) {
-                Node* temp = leftRotation();
-                temp->leftSon = remove(pos);
-            } else {
-                Node* temp = rightRotation();
-                temp->leftSon = remove(pos);
-            }
-
-            delete this;
-            return temp;
+    template<typename K>
+    Node* insert_range(K v){
+        Node* temp = this;
+        for (int i : v){
+            temp = temp->insert(i);
         }
-        return this;
+        return temp;
     }
 
-    unsigned int get(size_t pos){
-        // pos should always be in the tree
+    // Assuming the tree contains pos
+    Node* remove(size_t pos){
+        std::stack<Node*> path;
+        Node* current = this;
+        while (current->val != pos){
+            path.push(current);
+            if (pos < current->val){
+                current = current->leftSon;
+            } else if (pos > current->val) {
+                current = current->rightSon;
+            }
+        }
+
+        Node* temp = current;
+
+
+            if (current->rightSon != nullptr){
+                path.push(current);
+                current = current->rightSon;
+                while (current->leftSon != nullptr) {
+                    path.push(current);
+                    current = current->leftSon;
+                }
+                if (path.top()->rightSon != current){
+                    path.top()->leftSon = current->rightSon;
+                } else {
+                    path.top()->rightSon = current->rightSon;
+                }
+
+                temp->val = current->val;
+                delete current;
+            } else if (current->leftSon != nullptr){
+                path.push(current);
+                current = current->leftSon;
+                while (current->rightSon != nullptr) {
+                    path.push(current);
+                    current = current->rightSon;
+                }
+                if (path.top()->leftSon != current){
+                    path.top()->rightSon = current->leftSon;
+                } else {
+                    path.top()->leftSon = current->leftSon;
+                }
+                temp->val = current->val;
+                delete current;
+            }
+
+
+
+        while (path.size() != 1){
+            current = path.top();
+            current->calculateHeight();
+            path.pop();
+            if (path.top()->leftSon == current){
+                path.top()->leftSon = current->balanceTree();
+            } else if (path.top()->rightSon == current){
+                path.top()->rightSon = current->balanceTree();
+            }
+        }
+        return balanceTree();
+    }
+
+    Node* get_node(size_t pos){
         if (pos < val){
             if (leftSon != nullptr){
-                return leftSon->get(pos);
+                return leftSon->get_node(pos);
             }
             throw std::invalid_argument("Nie ma w drzewie");
         } else if (pos > val) {
             if (rightSon != nullptr){
-                return rightSon->get(pos);
+                return rightSon->get_node(pos);
             }
             throw std::invalid_argument("Nie ma w drzewie");
         } else {
-            return val;
+            return this;
         }
+    }
+
+    unsigned int get(size_t pos){
+        // pos should always be in the tree
+        return get_node(pos)->val;
+    }
+
+    void print_infix(){
+        if (leftSon != nullptr){
+            leftSon->print_infix();
+        }
+        std::cout << val << std::endl;
+        if (rightSon != nullptr){
+            rightSon->print_infix();
+        }
+    }
+
+    void update_heights(){
+        if (leftSon != nullptr){
+            leftSon->calculateHeight();
+        }
+        if (rightSon != nullptr){
+            rightSon->calculateHeight();
+        }
+        calculateHeight();
+    }
+
+    void check_avl(){
+        if (leftSon != nullptr){
+            leftSon->check_avl();
+        }
+        if (rightSon != nullptr){
+            rightSon->check_avl();
+        }
+        assert(bFactor() >= -1 && bFactor() <= 1);
     }
 };
 
-
 int main(void){
+    size_t test = 200;
+    //std::cin >> test;
+    std::vector<size_t> a;
+    for(int i = 1; i < test; i++){
+        a.push_back(i);
+    }
+    std::shuffle(a.begin(), a.end(), std::default_random_engine(4577645674));
+    Node* n = new Node(0);
+    for (auto val : a){
+        n = n->insert(val);
+    }
+    std::shuffle(a.begin(), a.end(), std::default_random_engine(46776765));
+    for (auto val : a){
+        n = n->remove(val);
+        n->update_heights();
+        n->check_avl();
+        assert(n->height <= 20);
+    }
     return 0;
 }
--- dyn.cpp
+++ dyn.cpp
@@ -1,43 +1,36 @@
 #include <iostream>
 #include <stdexcept>
+#include <queue>
+#include <iostream>
+#include <ranges>
+#include <list>
+#include <algorithm>
+#include <random>
+#include <stack>
+#include <cassert>
 
 class Node{
 public:
+    /*
+     * leftSon->val < val < righjtSon-val
+     */
     Node* leftSon;
     Node* rightSon;
-    size_t minIndex;
-    size_t indexOverride;
-    unsigned int x;
-    unsigned int count;
-    size_t height;
-    Node(size_t minIndex, unsigned int x, unsigned int count) : leftSon(nullptr), rightSon(nullptr), minIndex(minIndex),
-    x(x), count(count), height(0), indexOverride(0) {};
+    size_t val;
+    int height;
+    Node(size_t val) : leftSon(nullptr), rightSon(nullptr), val(val), height(1){};
 
     void calculateHeight(){
-        height = 1 + std::max(rightSon->height, leftSon->height);
+        height = 1 + std::max(rightSon == nullptr ? 0 : rightSon->height, leftSon == nullptr ? 0 : leftSon->height);
     }
 
-    size_t bFactor(){
-        return rightSon->height - leftSon->height;
-    }
-
-    void fixIndexingInRotation(Node* newParent){
-        if (newParent->leftSon != nullptr){
-            newParent->leftSon->indexOverride += newParent->indexOverride;
-        }
-
-        if (newParent->rightSon != nullptr){
-            newParent->rightSon->indexOverride += newParent->indexOverride;
-        }
-
-        newParent->minIndex += rightSon->indexOverride;
-        newParent->indexOverride = 0;
+    int bFactor(){
+        return (leftSon == nullptr ? 0 : leftSon->height) - (rightSon == nullptr ? 0 : rightSon->height);
     }
 
     Node* leftRotation(){
         Node* temp1 = rightSon->leftSon;
         Node* newParent = rightSon;
-        fixIndexingInRotation(newParent);
         rightSon->leftSon = this;
         rightSon = temp1;
         this->calculateHeight();
@@ -48,7 +41,6 @@ public:
     Node* rightRotation(){
         Node* temp1 = leftSon->rightSon;
         Node* newParent = leftSon;
-        fixIndexingInRotation(newParent);
         leftSon->rightSon = this;
         leftSon = temp1;
         this->calculateHeight();
@@ -56,38 +48,195 @@ public:
         return newParent;
     }
 
-    size_t maxPos(size_t override = 0){
-        if (this->leftSon != nullptr){
-            return leftSon->maxPos(override + indexOverride);
+    Node* doubleRightRotation(){
+        leftSon = leftSon->leftRotation();
+        return this->rightRotation();
+    }
+
+    Node* doubleLeftRotation(){ //might be a problem with nulls
+        rightSon = rightSon->rightRotation();
+        return this->leftRotation();
+    }
+
+    Node* balanceTree(){
+        if (bFactor() == -2 && rightSon->bFactor() <= 0){
+            return leftRotation();
+        } else if (bFactor() == -2 && rightSon->bFactor() > 0){
+            return  doubleLeftRotation();
+        } else if (bFactor() == 2 && leftSon->bFactor() >= 0){
+            return rightRotation();
+        } else if (bFactor() == 2 && leftSon->bFactor() < 0){
+            return doubleRightRotation();
+        } else return this;
+    }
+
+    size_t max(){
+        if (this->rightSon != nullptr){
+            return rightSon->max();
         }
-        return minIndex + count + indexOverride;
+        return val;
     }
 
-    Node* remove(size_t pos){
-        /* algorithm for removing*/
+    size_t min(){
+        if (this->leftSon != nullptr){
+            return leftSon->min();
+        }
+        return val;
     }
 
-    void insert(Node* node){
-        /*
-         * 1. Find the interval containing the index
-         * 2. Extend the interval to the point that it can contain its og self and inserted interval
-         * 3. Iterate back to the root adding the difference of the interval size to nodes to the right of the node
-         * 4. Remove the node
-         * 5. Split the node and insert all three parts with the standard algorithm
-         */
+    Node* insert(size_t pos){
+        return insert(new Node(pos));
+    }
+
+    Node* insert(Node* node){
+        if (node->val == this->val) {
+            throw std::invalid_argument("Wartosc istnieje");
+        }
+        if (node->val < this->val){
+            if (leftSon == nullptr){
+                leftSon = node;
+                calculateHeight();
+                return this;
+            } else {
+                leftSon = leftSon->insert(node);
+            }
+            calculateHeight();
+            return balanceTree();
+        } else {
+            if(rightSon == nullptr){
+                rightSon = node;
+                calculateHeight();
+                return this;
+            } else {
+                rightSon = rightSon->insert(node);
+            }
+            calculateHeight();
+            return balanceTree();
+        }
+        calculateHeight();
+        return this;
     };
 
-
-
-    unsigned int get(size_t pos, size_t override = 0){
-        // pos should always be in the tree
-        if (pos < minIndex + override){
-            return leftSon->get(pos, override + this->indexOverride);
-        } else if (pos >= minIndex + override + count) {
-            return rightSon->get(pos, override + this->indexOverride);
-        } else {
-            return x;
+    template<typename K>
+    Node* insert_range(K v){
+        Node* temp = this;
+        for (int i : v){
+            temp = temp->insert(i);
         }
+        return temp;
+    }
+
+    // Assuming the tree contains pos
+    Node* remove(size_t pos){
+        std::stack<Node*> path;
+        Node* current = this;
+        while (current->val != pos){
+            path.push(current);
+            if (pos < current->val){
+                current = current->leftSon;
+            } else if (pos > current->val) {
+                current = current->rightSon;
+            }
+        }
+
+        Node* temp = current;
+
+
+        if (current->rightSon != nullptr){
+            path.push(current);
+            current = current->rightSon;
+            while (current->leftSon != nullptr) {
+                path.push(current);
+                current = current->leftSon;
+            }
+            if (path.top()->rightSon != current){
+                path.top()->leftSon = current->rightSon;
+            } else {
+                path.top()->rightSon = current->rightSon;
+            }
+
+            temp->val = current->val;
+            delete current;
+        } else if (current->leftSon != nullptr){
+            path.push(current);
+            current = current->leftSon;
+            while (current->rightSon != nullptr) {
+                path.push(current);
+                current = current->rightSon;
+            }
+            if (path.top()->leftSon != current){
+                path.top()->rightSon = current->leftSon;
+            } else {
+                path.top()->leftSon = current->leftSon;
+            }
+            temp->val = current->val;
+            delete current;
+        }
+
+
+
+        while (path.size() != 1){
+            current = path.top();
+            current->calculateHeight();
+            path.pop();
+            if (path.top()->leftSon == current){
+                path.top()->leftSon = current->balanceTree();
+            } else if (path.top()->rightSon == current){
+                path.top()->rightSon = current->balanceTree();
+            }
+        }
+        return balanceTree();
+    }
+
+    Node* get_node(size_t pos){
+        if (pos < val){
+            if (leftSon != nullptr){
+                return leftSon->get_node(pos);
+            }
+            throw std::invalid_argument("Nie ma w drzewie");
+        } else if (pos > val) {
+            if (rightSon != nullptr){
+                return rightSon->get_node(pos);
+            }
+            throw std::invalid_argument("Nie ma w drzewie");
+        } else {
+            return this;
+        }
+    }
+
+    unsigned int get(size_t pos){
+        // pos should always be in the tree
+        return get_node(pos)->val;
+    }
+
+    void print_infix(){
+        if (leftSon != nullptr){
+            leftSon->print_infix();
+        }
+        std::cout << val << std::endl;
+        if (rightSon != nullptr){
+            rightSon->print_infix();
+        }
+    }
+
+    void update_heights(){
+        if (leftSon != nullptr){
+            leftSon->calculateHeight();
+        }
+        if (rightSon != nullptr){
+            rightSon->calculateHeight();
+        }
+        calculateHeight();
+    }
+
+    void check_avl(){
+        if (leftSon != nullptr){
+            leftSon->check_avl();
+        }
+        if (rightSon != nullptr){
+            rightSon->check_avl();
+        }
+        assert(bFactor() >= -1 && bFactor() <= 1);
     }
 };
 
